1608dd492cc1371e3005776a96291ebb36dc0c52
Gautham248 <95111246+Gautham248@users.noreply.github.com>
2021-11-27 05:28:48 -0800

    Paliendrome Program

---
+++ MODULE2/src/String_programs/PaliendromeProgram.java
@@ -0,0 +1,24 @@
+package String_programs;
+import java.util.Scanner;
+public class PaliendromeProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the string:  ");
+		String str=sc.nextLine();
+		String revstr="";
+		char ch;
+		for(int i=0;i<str.length();i++)
+		{
+			ch=str.charAt(i);
+			revstr=ch+revstr;
+		}
+		if(str.equals(revstr))
+			System.out.println("The String is a Paliendrome");
+		else
+			System.out.println("The String is not a Paliendrome");
+
+	}
+
+}
