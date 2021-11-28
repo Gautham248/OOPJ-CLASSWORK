1c0df6b68999578cddef13b286bc8db980bf4bda
Gautham248 <95111246+Gautham248@users.noreply.github.com>
2021-11-28 14:18:58 -0800

    Square Pattern

---
+++ PATTERN_PRINTING/src/Simple_Patterns/Square_Pattern.java
@@ -0,0 +1,19 @@
+package Simple_Patterns;
+
+public class Square_Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		for(int row=0;row<=n;row++)
+		{
+			for(int col=0;col<=n;col++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println("\n");
+		}
+
+	}
+
+}
