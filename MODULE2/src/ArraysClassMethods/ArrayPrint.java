41b0321254acf1a67c32ae947fc8b8b1f16bd7cf
Gautham248 <95111246+Gautham248@users.noreply.github.com>
2021-11-27 22:54:02 -0800

    ArrayPrint using enhanced for loop

---
+++ MODULE2/src/ArraysClassMethods/ArrayPrint.java
@@ -0,0 +1,13 @@
+package ArraysClassMethods;
+
+public class ArrayPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] cars= {"Volvo","BMW","Ford","Mazda"};
+		for(String car:cars)
+			System.out.println(car);
+
+	}
+
+}
