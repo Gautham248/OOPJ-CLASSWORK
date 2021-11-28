5407e859fe1004299fd48741976383422372c519
Gautham248 <95111246+Gautham248@users.noreply.github.com>
2021-11-27 18:57:31 -0800

    Pattern "JAVA"

---
+++ MODULE2/src/Basic_Program/JavaPatt.java
@@ -0,0 +1,14 @@
+package Basic_Program;
+
+public class JavaPatt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("    J     AA      V      V     AA      ");
+		System.out.println("    J    A  A      V    V     A  A      ");
+		System.out.println(" J  J   AAAAAA      V  V     AAAAAA      ");
+		System.out.println("  JJ   A      A      VV     A      A     ");
+
+	}
+
+}
