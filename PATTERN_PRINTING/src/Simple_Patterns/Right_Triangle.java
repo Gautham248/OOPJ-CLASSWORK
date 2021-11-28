5e48ec1d59fa3b18c0b420dbd6f26cb555172f9b
Gautham248 <95111246+Gautham248@users.noreply.github.com>
2021-11-28 14:18:44 -0800

    Right Triangle Pattern

--- PATTERN_PRINTING/src/Simple_Patterns/Right_Triangle.java
+++ PATTERN_PRINTING/src/Simple_Patterns/Right_Triangle.java
@@ -1,10 +1,27 @@
 package Simple_Patterns;
-
+import java.util.*;
 public class Right_Triangle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int num=4;
+		
+		//PATTERN
+		for(int row=0;row<=num;row++)//OUTER FOR LOOP->NUMBER OF ROWS
+		{
+			//FOR EVERY ROW RUN THE COLUMN
+		for(int column=0;column<=row;column++)	
+		{
+			//PRINT 
+			System.out.print("* ");
+		}
+		System.out.println("\n");
+			
+			
+		
+	
+		}
 
-	}
-
+}
 }
