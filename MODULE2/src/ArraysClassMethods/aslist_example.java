563aebce26aa9aeb643618745a118cccb8813b21
Gautham248 <95111246+Gautham248@users.noreply.github.com>
2021-11-27 22:52:56 -0800

    ArrayClass- asList();

---
+++ MODULE2/src/ArraysClassMethods/aslist_example.java
@@ -0,0 +1,14 @@
+package ArraysClassMethods;
+import java.util.Arrays;
+public class aslist_example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// int intarr[]= {10,20,15,22,35};
+		//convert elements as list
+		char ch[]= {'a','b','c','d'};
+		System.out.println("Integer Array List: "+Arrays.asList(ch));
+	
+
+}
+}
