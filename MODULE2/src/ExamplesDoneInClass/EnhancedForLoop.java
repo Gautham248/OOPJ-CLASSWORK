fca3ceb7bd7fdafef3ab28088ec26585e9574e8f
Gautham248 <95111246+Gautham248@users.noreply.github.com>
2021-11-27 22:52:24 -0800

    Enhanced For Loop

---
+++ MODULE2/src/ExamplesDoneInClass/EnhancedForLoop.java
@@ -0,0 +1,20 @@
+package ExamplesDoneInClass;
+
+public class EnhancedForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//SYNTAX:
+		//for(declaration:expression) {
+		//	statements
+		//}
+		//for(typr var:array)
+		//{
+		//	code
+		//}
+		
+		int array[]= {12,34,44,56,78};
+		for(int i:array)
+			System.out.println(i+"\n");
+}
+}
