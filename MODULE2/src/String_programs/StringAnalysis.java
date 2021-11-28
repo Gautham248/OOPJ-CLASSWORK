1af696a01d620e8b9f4054763505bc843b90831d
Gautham248 <95111246+Gautham248@users.noreply.github.com>
2021-11-27 19:56:38 -0800

    Analyzing number of alphabets,vowels,consonants,numbers,space and special characters in a string

---
+++ MODULE2/src/String_programs/StringAnalysis.java
@@ -0,0 +1,40 @@
+package String_programs;
+import java.util.*;
+
+public class StringAnalysis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the string:  ");
+		String str=sc.nextLine();
+		char ch;
+		int whitespace=0,alphabet=0,special_character=0,vowel=0,consonant=0,numbers=0;
+		str=str.toLowerCase();
+		for(int i=0;i<str.length();i++)
+		{
+			ch=str.charAt(i);
+			if(ch==' ')
+				whitespace++;
+			else if(ch>='a'&&ch<='z')
+			{
+				alphabet++;
+				if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+					vowel++;
+				else
+					consonant++;
+			}
+			else if(ch>='0'&&ch<='9')
+				numbers++;
+			else 
+				special_character++;
+	}
+		System.out.println("WhiteSpace:  "+whitespace);
+		System.out.println("Alphabets:  "+alphabet);
+		System.out.println("Vowels: "+vowel);
+		System.out.println("Consonants: "+consonant);
+		System.out.println("SpecialCharacters: "+special_character);
+		System.out.println("Numbers: "+numbers);
+
+}
+}
