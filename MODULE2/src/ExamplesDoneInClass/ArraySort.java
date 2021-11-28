2c14374e0da592eab78cfabf0ca3792706007dc2
Gautham248 <95111246+Gautham248@users.noreply.github.com>
2021-11-27 22:53:10 -0800

    Array Sort

---
+++ MODULE2/src/ExamplesDoneInClass/ArraySort.java
@@ -0,0 +1,15 @@
+package ExamplesDoneInClass;
+import java.util.Arrays;
+
+public class ArraySort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int intarr[]= {10,20,15,22,35};
+		Arrays.sort(intarr);
+		System.out.println("The Sorted array is:  ");
+		for(int element:intarr)
+			System.out.println(element+"\n");
+	}
+
+}
